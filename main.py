01eb13a151cef1c3fb6186e0d4532402c7044b0b
raushansuri2 <rksuri2@gmail.com>
2023-09-12 07:36:44 +0100

    n

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-# This is a sample Python script.
+import streamlit as st
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+st.set_page_config(layout="wide")
 
+col1, col2 = st.columns(2)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+with col1:
+    st.image("images/photo.png")
 
+with col2 :
+    st.title("About My self Raushan")
+    content = """
+    sfjksahdjf sabasjdf habfjasf asjdhfahfsagfahsfas jhsdgfjhsagfagf hjvajsdf
+    basfhsad
+    asdhjsagfas
+    asbhjasbfas
+    """
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    st.write(content)
